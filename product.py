06f109c745336b7a8d318fd0d6bb17c63c60c953
Renuka <renuka.rabbanagoudar@gmail.com>
2025-12-11 20:38:47 +0530

    Update product.py

--- product.py
+++ product.py
@@ -4,7 +4,7 @@ def get_product_info(product_id, name, quantity, price):
         f"ID: {product_id}\n"
         f"Name: {name}\n"
         f"Quantity: {quantity}\n"
-        f"Price: {price}"
+        f"Price: {price:.2f}"
     )
 if __name__ == "__main__":
     print(get_product_info(101, "Laptop", 5, 55000.50))
